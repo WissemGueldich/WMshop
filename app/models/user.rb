7dd3db94ad1282369158978458d9fdff75c20661
WissemGueldich <gueldich.wissem@esprit.tn>
2021-08-21 22:11:10 +0100

    added tailwind and integrated views for registration:new/edit, session:new, password:new

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates_numericality_of :age, :greater_than_or_equal_to => 18
-
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_141422) do
+ActiveRecord::Schema.define(version: 2021_08_21_203119) do
 
   create_table "products", force: :cascade do |t|
     t.string "image"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_08_20_141422) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
