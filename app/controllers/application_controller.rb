6ffeeb1e2517ec81d122cb73b7f758120c115ef2
majd <fazaamajdfazaa@gmail.com>
2021-08-23 14:01:46 +0100

    create shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,27 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if:  :devise_controller?
+    protect_from_forgery with: :exception
 
-    protected
+    before_action :current_cart
+
+  def current_cart
+    @current_cart ||= ShoppingCart.new(token: cart_token)
+  end
+  helper_method :current_cart
+
+  private
+
+  def cart_token
+    return @cart_token unless @cart_token.nil?
+
+    session[:cart_token] ||= SecureRandom.hex(8)
+    @cart_token = session[:cart_token]
+  end
+
+  protected
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:age])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name,:age])
-
     end
 end
+
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,4 @@
 class Category < ApplicationRecord
     has_many :product_categories
     has_many :products, through: :product_categories
-    belongs_to :product
 end
