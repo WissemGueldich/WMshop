d9577df7e21d088f1652781ef1a0393c9e7fbb78
majd <fazaamajdfazaa@gmail.com>
2021-08-27 09:01:16 +0100

    prducts font-end

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,10 @@ class Product < ApplicationRecord
     validates :description, presence: true
 
     belongs_to :category
-    has_many_attached :images
+    has_one_attached :image
+
+    def thumbnail 
+        return self.image.variant(resize: '350x350!').processed
+    end
+
 end
